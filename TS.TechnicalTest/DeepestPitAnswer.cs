c634eaf4a039e52332742eaab675fb4f595f8dec
Kondelelani Nedzingahe <knedzingahe@astutefse.com>
2024-04-08 19:35:34 +0200

    Deepest pit first test

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,42 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        var pits = new List<Tuple<int, int, int>>();
+        var firstHigh = points[0];
+        var low = 0;
+        var secondHigh = 0;
+        var deepestPit = 0;
+        var isInPit = false;
+
+        for (var c=1; c<points.Length; c++)
+        {
+            if (points[c] > firstHigh && !isInPit)
+            {
+                firstHigh = points[c];
+            }
+            else if (points[c] < low)
+            {
+                low = points[c];
+                isInPit = true;
+            }
+            else if (points[c] > secondHigh && isInPit)
+            {
+                secondHigh = points[c];
+
+                var pit = new Tuple<int, int, int>(firstHigh, low, secondHigh);
+                pits.Add(pit);
+            }
+        }
+
+        foreach (var pit in pits)
+        {
+            var minDiff = Math.Min(pit.Item1-pit.Item2, pit.Item3-pit.Item2);
+
+            if (deepestPit < minDiff)
+            {
+                deepestPit = minDiff;
+            }
+        }
+         return deepestPit;
     }
 }
